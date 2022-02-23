9ba82ede99ea5b335fe4ebd3d43fbada1059c384
RohmanBenyRiyanto <rendyasw@gmail.com>
2022-02-23 18:12:38 +0700

    📋Tugas Beny Alterra Academy

---
+++ 04_Introduction to Algorithm and Dart Programming/praktikum/praktikum_introduction_to_algorithm_and_dart_programming/bin/tugas_nomor_2.dart
@@ -0,0 +1,7 @@
+void main() {
+  String? firstName = 'Rohman';
+  String? lastName = 'Beny';
+  String? belajar = 'Flutter';
+
+  print('Hello nama saya adalah $firstName $lastName dan saya sedang belajar $belajar');
+}
