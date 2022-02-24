2e2c3c2894e543d4f697102f800ae8ccfc4d69cf
RohmanBenyRiyanto <rendyasw@gmail.com>
2022-02-24 17:21:06 +0700

    📋Tugas Beny Alterra Academy

---
+++ 05_Basic Dart Programming/praktikum/praktikum_basic_dart_programming/bin/tugas_nomor_1.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'dart:math' show pi;
+
+/*Value berdasarkan value yang sudah
+  di deklarasikan*/
+// void main() {
+//   double value, area;
+//   value = 10;
+
+//   area = pi * value * value;
+//   print('Luas lingkaran adalah $area');
+// }
+
+//Value berdasarkan Inputan dari console
+void main() {
+  double area;
+
+  print('Masukkan panjang jari-jari lingkaran: ');
+  var num1 = stdin.readLineSync();
+  var num2 = double.parse(num1!);
+
+  area = pi * num2 * num2;
+  print('Luas lingkaran adalah $area');
+}
