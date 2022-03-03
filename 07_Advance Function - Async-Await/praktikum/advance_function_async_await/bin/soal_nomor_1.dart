6489972c67e68d731b2ba424c374512a0c7c9d54
RohmanBenyRiyanto <rendyasw@gmail.com>
2022-03-03 16:27:39 +0700

    📋Tugas Beny Alterra Academy

--- 07_Advance Function - Async-Await/praktikum/advance_function_async_await/bin/soal_nomor_1.dart
+++ 07_Advance Function - Async-Await/praktikum/advance_function_async_await/bin/soal_nomor_1.dart
@@ -1,53 +1,31 @@
-// import 'dart:math';
-
-// void main() async {
-//   var randoms = Random();
-//   var nilaiRandom = randoms.nextInt(100);
-//   print('Nilai Pengali adalah : $nilaiRandom\n');
-
-//   Future<List>? _futureOfList = _getList();
-//   List? list = await _futureOfList;
-//   print('List awal adalah $list\n');
-
-//   var pengali = list.map((number) => number * nilaiRandom);
-
-//   print('Hasil dari $nilaiRandom x $list adalah : $pengali\n');
-
-//   var newList = List.from(list)..addAll(pengali);
-
-//   print('List terbaru adalah : $newList');
-// }
-
-// Future<List> _getList() async {
-//   await Future.delayed(Duration(seconds: 1));
-//   return Future.value([1, 2, 3, 4, 5]);
-// }
-
-import 'dart:async';
 import 'dart:math';
 
-Future<bool> proses() async {
-  await Future.delayed(Duration(seconds: 1));
-  var randoms = Random();
-  var nilaiRandom = randoms.nextInt(100);
-  print('Nilai Pengali adalah : $nilaiRandom\n');
-  var numbers = <int>[randoms.nextInt(100)];
-  print('List awal adalah $numbers\n');
+void main() async {
+  Future<List>? _futureOfList = _getList();
+  List? list = await _futureOfList;
+  var nilaipengali = Random().nextInt(100);
 
-  for (var e in numbers) {
-    // var pengali = numbers.map((number) => e * nilaiRandom);
-    var pengali = e * nilaiRandom;
-    print('Hasil dari $e dikalikan $nilaiRandom adalah : $pengali\n');
-
-    var newList = List.from(numbers)..add(pengali);
-    print('List terbaru adalah : $newList\n');
-  }
-
-  return true;
+  print(await fungsi(list, nilaipengali));
 }
 
-void main() async {
-  print('Mulai.....\n');
-  await proses();
-  print('Selesai...');
+Future<List> _getList() async {
+  await Future.delayed(Duration(seconds: 1));
+  return Future.value(List.generate(5, (number) => Random().nextInt(100)));
+}
+
+Future<String> fungsi(List data, int pengali) async {
+  print('Proses Start\n');
+  print('Nilai Pengali adalah : $pengali\n');
+  print('List awal adalah $data\n');
+  var kali = data.map((number) => number * pengali);
+
+  for (var e in data) {
+    var currentElement = e;
+    var currentKali = currentElement * pengali;
+    print('Hasil dari $pengali x $currentElement adalah : $currentKali\n');
+  }
+
+  var newList = List.from(data)..addAll(kali);
+  print('List terbaru adalah : $newList');
+  return '\nProses End';
 }
--- 07_Advance Function - Async-Await/praktikum/advance_function_async_await/bin/test.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-
-void main() {
-  stdout.write("Enter your name : ");
-  String? name = stdin.readLineSync()!;
-  stdout.write(name);
-}
