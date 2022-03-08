1cf3a82ae9b7f443ed2d4d0106805602369a1cb6
RohmanBenyRiyanto <rendyasw@gmail.com>
2022-03-08 15:40:07 +0700

    📋Tugas Beny Alterra Academy

---
+++ 09_Dart Object Oriented Programming 1/praktikum/dart_object_oriented_programming_1/bin/nomor_2.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+class Hewan {
+  double berat = 100.0;
+}
+
+class Mobil {
+  List hewan = [
+    'Kucing',
+    'Anjing',
+    'Kelinci',
+    'Kerbau',
+  ];
+  int max = 5;
+
+  void tambahMuatan() {
+    if (hewan.length < max) {
+      stdout.write('Masukkan nama hewan : ');
+      String? tambah = stdin.readLineSync()!;
+      hewan.add(tambah);
+      print('Berhasil menambahkan $tambah\n');
+      print('Daftar hewan Terbaru : $hewan\n');
+    } else if (hewan.length >= max) {
+      print('Muatan telah penuh (MAX 5)!\n');
+    } else {
+      print('Error\n');
+    }
+  }
+
+  void totalMuatan() {
+    print('Total Muatan adalah ${hewan.length}\n');
+  }
+
+  void lanjut() {
+    stdout.write('Tambah muatan lagi?(y/n) : ');
+    String? button = stdin.readLineSync()!;
+    if (button == 'y') {
+      if (hewan.length < max) {
+        stdout.write('Masukkan nama hewan : ');
+        String? tambah = stdin.readLineSync()!;
+        hewan.add(tambah);
+        print('Berhasil menambahkan $tambah\n');
+        print('Daftar hewan Terbaru : $hewan\n');
+        print('Total Muatan adalah ${hewan.length}\n');
+      } else if (hewan.length >= max) {
+        print('Muatan telah penuh (MAX 5)!\n');
+      }
+    } else if (button == 'n') {
+      print('Program telah selesai!\n');
+    } else {
+      print('error\n');
+    }
+  }
+}
+
+void main() {
+  var mobils = Mobil();
+
+  print('Total Berat hewan adalah : ${Hewan().berat}');
+  print('Daftar hewan : ${mobils.hewan}\n');
+
+  stdout.write('Menambahkan muatan?(y/n) : ');
+  String? button = stdin.readLineSync()!;
+  if (button == 'y') {
+    // Menambahkan nilai ke dalam list
+    mobils.tambahMuatan();
+    mobils.totalMuatan();
+    mobils.lanjut();
+  } else if (button == 'n') {
+    print('Program telah selesai!\n');
+  } else {
+    print('error\n');
+  }
+}
